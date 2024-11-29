7ed5af7b8821b02b42de2c719070e76be4c722e9
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-11-29 16:21:37 -0600

    Add user model (#18)
    Fixes #17

--- src/app/models.py
+++ src/app/models.py
@@ -5,3 +5,13 @@ class Task(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     title = database.Column(database.String(120))
     status = database.Column(database.String(20), default="Pending")
+
+
+class User(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(
+        database.String(120), unique=True, nullable=False
+    )
+    password = database.Column(
+        database.String(120), unique=True, nullable=False
+    )
