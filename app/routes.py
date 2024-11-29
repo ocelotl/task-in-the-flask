3c6d7e629a03bc42f5860f1e71d5f789e46eb84d
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-11-28 21:28:35 -0600

    Add blueprint comment

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from flask import Blueprint, jsonify
 from .models import Task
 
+# A Blueprint object works similarly to a Flask application object but it is
+# not actually an application, but a _blueprint_ of how to construct or
+# extend an application.
+# A Flask Blueprint is a set of operations which can be registered on an
+# application, even multiple times.
+# Blueprints can share application configuration and can change an application
+# object with being registered, but they cannot be unregistered without
+# destroying the application object.
+# Blueprints record operations that are executed when registered on an
+# application.
 bp = Blueprint('routes', __name__)
 
 
