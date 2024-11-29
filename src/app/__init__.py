a8b52c23f5a6beb7d991016a5139d3cca647c51e
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-11-29 10:27:38 -0600

    Make configuration optional in create_app (#13)
    This allows the app to be executed from the command line without having
    to pass a configuration path to the create_app function.
    
    Fixes #12

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -6,14 +6,15 @@ from .routes import blueprint
 database = SQLAlchemy()
 
 
-def create_app(configuration_filename):
+def create_app(configuration_filename=None):
     app = Flask(__name__)
 
-    # This loads the config objects from configuration_filename into the
-    # app.config attribute.
-    app.config.from_pyfile(configuration_filename)
+    if configuration_filename is not None:
+        # This loads the config objects from configuration_filename into the
+        # app.config attribute.
+        app.config.from_pyfile(configuration_filename)
+        database.init_app(app)
 
-    database.init_app(app)
     app.register_blueprint(blueprint)
 
     return app
