b73520fb23a5edc5c2d241615f27cb29278318c6
Diego Hurtado <ocelotl@users.noreply.github.com>
2024-11-28 21:44:48 -0600

    Add new route for task creation

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .models import Task
+from . import db
 
 # A Blueprint object works similarly to a Flask application object but it is
 # not actually an application, but a _blueprint_ of how to construct or
@@ -18,3 +19,16 @@ bp = Blueprint('routes', __name__)
 def get_tasks():
     tasks = Task.query.all()
     return jsonify([task.to_dict() for task in tasks])
+
+
+@bp.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.json
+    new_task = Task(
+        title=data['title'],
+        user_id=data['user_id'],
+        project_id=data['project_id']
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task created successfully!'})
